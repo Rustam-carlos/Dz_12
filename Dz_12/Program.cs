97e616fb868332dcd395bafd6178c361ed4c4711
Rustam Galeev <Vincent-cat@mail.ru>
2019-04-10 03:01:28 +0600

    Добавьте файлы проекта.

---
+++ Dz_12/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+namespace Dz_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer xmlSerialiser = new XmlSerializer(typeof(Tank));
+            File.Create("D:/1/1.bin");
+            BinaryFormatter serializer = new BinaryFormatter();
+            using (var stream = File.Open(@"D:/1/1.bin", FileMode.Truncate))
+            {
+                var tank = new Tank
+                {
+                    type = "Tiger H1",
+                    ammunition = 250,
+                    armor = 680,
+                    mobility = 40
+                };
+                serializer.Serialize(stream, tank);
+            }
+            using (var stream = File.Open(@"D:/1/1.bin", FileMode.Truncate))
+            {
+                var tank2 = new Tank
+                {
+                    type = "Tiger P2",
+                    ammunition = 250,
+                    armor = 680,
+                    mobility = 40
+                };
+                string data = JsonConvert.SerializeObject(tank2);
+                Tank result = JsonConvert.DeserializeObject<Tank>(data);
+            }
+        }
+    }
+}
---
+++ Dz_12/Tank.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_12
+{
+    public class Tank
+    {
+        public string type { get; set; }
+        public int ammunition { get; set; }
+        public int armor { get; set; }
+        public int mobility { get; set; }
+
+        public Tank()
+        {
+            type = "null";
+            ammunition = 0;
+            armor = 0;
+            mobility = 0;
+
+        }
+        public Tank(string str, int x, int y, int z)
+        {
+            type = str;
+            ammunition = x;
+            armor = y;
+            mobility = z;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Танк \t\t\t\t- " + type);
+            Console.WriteLine("Колличество боеукладки\t\t - " + ammunition);
+            Console.WriteLine("Ссумарный коэфицент брони\t - " + armor);
+            Console.WriteLine("Ссумарный коэфицент маневренности - " + mobility);
+            Console.WriteLine("---------------------------------");
+        }
+        
+    }
+}
